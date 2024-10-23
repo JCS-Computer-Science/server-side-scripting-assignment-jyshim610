0738af3951274bcdf6cd6bef848b8df16ea2059c
jyshim610 <jyshim610@gmail.com>
2024-10-23 14:37:21 -0600

    begin

--- server.js
+++ server.js
@@ -2,10 +2,35 @@ const express = require("express");
 const uuid = require("uuid")
 const server = express();
 server.use(express.json())
+server.use(express.static('public'))
 
 
 //All your code goes here
 let activeSessions={}
+
+server.get('/newgame', (req, res) => {
+    let newID = uuid.v4()
+    let newgame = {
+        wordToGuess: "spike",
+        guesses: [],
+        wrongLetters: [],
+        closeLetters: [],
+        rightLetters: [],
+        remainingGuesses: 6,
+        gameOver: false
+    }
+    activeSessions[newID] = newgame
+    res.status(201)
+    res.send({ sessionID: newID })
+})
+server.get('/gamestate', (req, res) => {
+
+
+
+    
+})
+
+
 
 //Do not remove this line. This allows the test suite to start
 //multiple instances of your server on different ports

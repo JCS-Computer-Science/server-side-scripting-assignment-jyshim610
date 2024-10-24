65bb2988bb9520a8da4cdb3d0b5ba1dde343ad41
Junyong Shim <1100079683@edu.cbe.ab.ca>
2024-10-24 13:56:45 -0600

    npm run serve

--- server.js
+++ server.js
@@ -24,12 +24,12 @@ server.get('/newgame', (req, res) => {
     res.send({ sessionID: newID })
 })
 server.get('/gamestate', (req, res) => {
- //   const sessionID = req.query.sessionID
- //   const gamestate = activeSessions[sessionID]
- //   if(!gamestate){
- //       return res.status(404).send({error: "Invalid session ID"})
- //   }
- //   res.status(200).send(gamestate)
+   const sessionID = req.query.sessionID
+   const gamestate = activeSessions[sessionID]
+   if(!gamestate){
+       return res.status(404).send({error: "Invalid session ID"})
+   }
+   res.status(200).send(gamestate)
 
 
 })

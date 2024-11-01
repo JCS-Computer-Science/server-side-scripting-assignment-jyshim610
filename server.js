9f96546a2ea20964be0e5f61cbe0b69e7e286200
jyshim610 <jyshim610@gmail.com>
2024-11-01 12:47:06 -0600

    server.delete .delete done

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ let activeSessions={}
 server.get('/newgame', (req, res) => {
     let wordToGuess = req.query.answer || "spike"
     let newID = uuid.v4()
+    if(!wordToGuess){
+        wordToGuess = undefined
+    }
     let newgame = {
         wordToGuess: "spike",
         guesses: [],
@@ -19,19 +22,18 @@ server.get('/newgame', (req, res) => {
         gameOver: false
     }
     activeSessions[newID] = newgame
-    res.status(201).send({ sessionID: newID })
+    return res.status(201).send({ sessionID: newID })
 })
 server.get('/gamestate', (req, res) => {
     let ID = req.query.sessionID
     if(!ID){
-        res.status(400)
-        res.send({error: "Invalid ID"})
+        res.status(400).send({error: "Invalid ID"})
+    }
+    if(activeSessions[ID]){
+        let gameState = activeSessions[ID]
+        res.status(200).send({gameState: activeSessions[ID]})
     }else{
-        if(activeSessions[ID]){
-            res.status(200).send({gameState: activeSessions[ID]})
-        }else{
-            res.status(404).send({error: "game does not exist"})
-        }
+        res.status(404).send({error: "game does not exist"})
     }
  })
 server.post('/guess', (req, res) => {
@@ -59,19 +61,68 @@ server.post('/guess', (req, res) => {
     let rightLetters = []
     let closeLetters = []
     let wrongLetters = []
-    for (let i = 0; i < array.length; i++) {
-        let guessedLetter = guess[i]
-        if(game.wordToGuess[i] === guessedLetter){
-            rightLetters.push(guessedLetter)
-        }else if(game.wordToGuess.includes(guessedLetter)){
-            closeLetters.push(guessedLetter)
-        }else{
-            wrongLetters.push(guessedLetter)
+
+    game.guesses.push(guess)
+
+    for (let i = 0; i < rightLetters.length; i++) {
+        let letter = rightLetters[i]
+        let found = false
+        for (let j = 0; j < game.rightLetters.length; j++) {
+            if(game.rightLetters[j] === letter){
+                found = true
+                break
+            }
+            
+        }
+        if(!found){
+            game.rightLetters.push(letter)
+        } 
+    }
+    for (let i = 0; i < closeLetters.length; i++) {
+        let letter = closeLetters[i]
+        let found = false
+        for (let j = 0; j < game.closeLetters.length; j++) {
+            if(game.closeLetters[j] === letter){
+                found = true
+                break
+            }  
+        }
+        if(!found){
+            game.closeLetters.push(letter)
+        }   
+    }for (let i = 0; i < wrongLetters.length; i++) {
+        let letter = wrongLetters[i]
+        let found = false
+        for (let j = 0; j < game.wrongLetters.length; j++) {
+            if(game.wrongLetters[j] === letter){
+                found = true
+                break
+            }
+            
+        }
+        if(!found){
+            game.wrongLetters.push(letter)
         }
     }
-    
-        
-    
+    game.remainingGuesses--
+
+    if(guess === game.wordToGuess){
+        game.gameOver = true
+        return res.status(200).send({
+            message: "You won!",
+            gameState: game         
+        })
+    }else if(game.remainingGuesses === 0){
+        game.gameOver = true
+        return res.status(200).send({
+            message: "You lost!",
+            gameState: game
+        })
+    }
+    return res.status(201).send({
+        message: "guess recorded",
+        gameState: game
+    })
 })
 server.delete('/reset', (req,res) => {
     let ID = req.query.sessionID
@@ -91,6 +142,18 @@ server.delete('/reset', (req,res) => {
         gameOver: false
     }
     res.status(200).send({gameState: activeSessions[ID]})
+})
+server.delete('/delete', (req,res) => {
+    let ID = req.query.sessionID
+    if(!ID){
+        return res.status(400).send({error: "Session is required"})
+    }
+    if(!activeSessions[ID]){
+        return res.status(404).send({error: "Game session not found"})
+    }
+    delete activeSessions[ID]
+
+    res.status(204).send()
 })
 
 

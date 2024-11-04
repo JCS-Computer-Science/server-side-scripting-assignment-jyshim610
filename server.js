2b0a1681616dec2693089a654bf1cb1ef21324a8
jyshim610 <jyshim610@gmail.com>
2024-11-04 12:55:14 -0700

    All completed

--- server.js
+++ server.js
@@ -9,11 +9,8 @@ let activeSessions={}
 server.get('/newgame', (req, res) => {
     let wordToGuess = req.query.answer || "spike"
     let newID = uuid.v4()
-    if(!wordToGuess){
-        wordToGuess = undefined
-    }
     let newgame = {
-        wordToGuess: "spike",
+        wordToGuess: wordToGuess,
         guesses: [],
         wrongLetters: [],
         closeLetters: [],
@@ -29,12 +26,23 @@ server.get('/gamestate', (req, res) => {
     if(!ID){
         res.status(400).send({error: "Invalid ID"})
     }
-    if(activeSessions[ID]){
-        let gameState = activeSessions[ID]
-        res.status(200).send({gameState: activeSessions[ID]})
-    }else{
-        res.status(404).send({error: "game does not exist"})
+    let gameState = activeSessions[ID]
+    if(!gameState){
+        return res.status(404).send({error: "Game session not found"})
     }
+    if(!gameState.gameOver){
+        gameState.wordToGuess = undefined
+    }
+    let responseState = {
+        wordToGuess: gameState.gameOver ? gameState.wordToGuess: undefined,
+        guesses: gameState.guesses,
+        wrongLetters:gameState.wrongLetters,
+        closeLetters: gameState.closeLetters,
+        rightLetters: gameState.rightLetters,
+        remainingGuesses: gameState.remainingGuesses,
+        gameOver: gameState.gameOver
+    }
+    return res.status(200).send({gameState: responseState})
  })
 server.post('/guess', (req, res) => {
     let ID = req.body.sessionID
@@ -58,70 +66,96 @@ server.post('/guess', (req, res) => {
     if(game.gameOver){
         return res.status(400).send({error: "Game is over"})
     }
+    let guessResult = []
     let rightLetters = []
     let closeLetters = []
     let wrongLetters = []
 
-    game.guesses.push(guess)
+    for (let i = 0; i < 5; i++) {
+        let letter = guess[i]
+        let result = "WRONG"
+
+        if(game.wordToGuess[i] === letter){
+            result = "RIGHT"
+            rightLetters.push(letter)
+        }else if(game.wordToGuess.includes(letter)){
+            result = "CLOSE"
+            closeLetters.push(letter)
+        }else{
+            wrongLetters.push(letter)
+        }
+        guessResult.push({value: letter, result: result})
+    }
+    game.guesses.push(guessResult)
 
     for (let i = 0; i < rightLetters.length; i++) {
-        let letter = rightLetters[i]
-        let found = false
-        for (let j = 0; j < game.rightLetters.length; j++) {
-            if(game.rightLetters[j] === letter){
-                found = true
-                break
-            }
-            
+        if(!game.rightLetters.includes(rightLetters[i])){
+            game.rightLetters.push(rightLetters[i])
         }
-        if(!found){
-            game.rightLetters.push(letter)
-        } 
+        
     }
+
     for (let i = 0; i < closeLetters.length; i++) {
-        let letter = closeLetters[i]
-        let found = false
-        for (let j = 0; j < game.closeLetters.length; j++) {
-            if(game.closeLetters[j] === letter){
-                found = true
-                break
-            }  
+        if(!game.closeLetters.includes(closeLetters[i]) && !game.closeLetters.includes(closeLetters)){
+            game.closeLetters.push(closeLetters[i])
         }
-        if(!found){
-            game.closeLetters.push(letter)
-        }   
-    }for (let i = 0; i < wrongLetters.length; i++) {
-        let letter = wrongLetters[i]
-        let found = false
-        for (let j = 0; j < game.wrongLetters.length; j++) {
-            if(game.wrongLetters[j] === letter){
-                found = true
-                break
-            }
-            
-        }
-        if(!found){
-            game.wrongLetters.push(letter)
-        }
+        
     }
+    game.closeLetters = game.closeLetters.filter(letter => !game.rightLetters.includes(letter))
+    
+    for (let i = 0; i < wrongLetters.length; i++) {
+        if(!game.wrongLetters.includes(wrongLetters[i])){
+            game.wrongLetters.push(wrongLetters[i])
+        }
+        
+    }
+    
+
+
+
+    
     game.remainingGuesses--
 
     if(guess === game.wordToGuess){
         game.gameOver = true
         return res.status(200).send({
             message: "You won!",
-            gameState: game         
+            gameState: {
+                wordToGuess: game.wordToGuess,
+                guesses: game.guesses,
+                wrongLetters: game.wrongLetters,
+                closeLetters: game.closeLetters,
+                rightLetters: game.rightLetters,
+                remainingGuesses: game.remainingGuesses,
+                gameOver: game.gameOver
+            }         
         })
     }else if(game.remainingGuesses === 0){
         game.gameOver = true
         return res.status(200).send({
             message: "You lost!",
-            gameState: game
+            gameState: {
+                wordToGuess: game.wordToGuess,
+                guesses: game.guesses,
+                wrongLetters: game.wrongLetters,
+                closeLetters: game.closeLetters,
+                rightLetters: game.rightLetters,
+                remainingGuesses: game.remainingGuesses,
+                gameOver: game.gameOver
+            }
         })
     }
     return res.status(201).send({
         message: "guess recorded",
-        gameState: game
+        gameState: {
+            wordToGuess: game.gameOver ? game.wordToGuess: undefined,
+            guesses: game.guesses,
+            wrongLetters: game.wrongLetters,
+            closeLetters: game.closeLetters,
+            rightLetters: game.rightLetters,
+            remainingGuesses: game.remainingGuesses,
+            gameOver: game.gameOver
+        }
     })
 })
 server.delete('/reset', (req,res) => {
@@ -141,7 +175,7 @@ server.delete('/reset', (req,res) => {
         remainingGuesses: 6,
         gameOver: false
     }
-    res.status(200).send({gameState: activeSessions[ID]})
+    return res.status(200).send({gameState: activeSessions[ID]})
 })
 server.delete('/delete', (req,res) => {
     let ID = req.query.sessionID
